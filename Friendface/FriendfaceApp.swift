1bc4148658aa99fc74793d61d889fbd971ec79bd
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-08-09 06:29:21 +0930

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Woodrow Martyr on 9/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
